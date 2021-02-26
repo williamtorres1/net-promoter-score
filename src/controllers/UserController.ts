d6a7a6b0023a466e83cfcb0b940e3d11dfd89b0e
William Torres <contatowilliamtorres@gmail.com>
2021-02-26 16:04:32 -0300

    Day 2 finalized. Check if user exists and return error.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -8,6 +8,14 @@ class UserController {
 
     const usersRepository = getRepository(User);
 
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExists) {
+      return response.status(400).json({ error: 'User already exists.' });
+    }
+
     const user = usersRepository.create({ name, email });
 
     await usersRepository.save(user);
